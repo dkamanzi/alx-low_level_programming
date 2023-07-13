de29fca72539359588a569740b02ff50a2ce8dbe
dkamanzi <ddkmanzi@gmail.com>
2023-07-13 10:02:40 -0700

    libmy.a

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,10 +1,9 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
-char *_strcat(char *dest, char *src)
+int _strlen(char *s)
 {
-return strcat(dest, src);
+return strlen(s);
 }
